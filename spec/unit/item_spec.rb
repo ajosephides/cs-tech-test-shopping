e89c09dee3c33fe7d383c6818e440a7e6325e491
Alexis Josephides <ajosephides@gmail.com>
2019-09-10 11:07:11 +0100

    tests: added tests to check price of other items

--- spec/unit/item_spec.rb
+++ spec/unit/item_spec.rb
@@ -4,12 +4,25 @@ require_relative '../../lib/item.rb'
 describe Item  do
   describe '#price' do
   
-    it 'correctly prices items in the basket' do
+    it 'correctly prices items in the basket - apple' do
       item = Item.new('apple')
       expect(item.price).to be(35)
     end
-  
+
+    it 'correctly prices items in the basket - banana' do
+      item = Item.new('banana')
+      expect(item.price).to be(20)
+    end
+
+    it 'correctly prices items in the basket - melon' do
+      item = Item.new('melon')
+      expect(item.price).to be(50)
+    end
+
+    it 'correctly prices items in the basket - lime' do
+      item = Item.new('lime')
+      expect(item.price).to be(15)
+    end
   
   end
-
 end

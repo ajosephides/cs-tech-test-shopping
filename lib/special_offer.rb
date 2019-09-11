cd3df65679f05c8bb46031330a8e9af96eecbfb6
Alexis Josephides <ajosephides@gmail.com>
2019-09-11 09:10:16 +0100

    specil offer: zero discount calculated

---
+++ lib/special_offer.rb
@@ -0,0 +1,32 @@
+class SpecialOffer
+
+  PRICE_LIST = {:apple => 35, :banana => 20, :melon => 50, :lime => 15 }
+  BUY_X_GET_1 = {:melon => 2, :lime => 3}
+
+  def initialize(basket)
+    @basket = basket
+    @basket_by_type = count_items(basket)
+  end
+
+
+  def discount
+    discount = 0
+    BUY_X_GET_1.keys.each do |offer|
+        discount = discount + ((@basket_by_type[offer] / BUY_X_GET_1[offer]) * PRICE_LIST[offer])
+    end
+    return discount
+  end
+
+
+  private
+
+  def count_items(basket)
+    basket_count = {}
+    basket.each do |item|
+      key = item.item.downcase.to_sym
+      basket_count.key?(key) ? basket_count[key] += 1 : basket_count[key] = 1
+    end
+    return basket_count
+  end
+
+end
---
+++ spec/unit/special_offers_spec.rb
@@ -0,0 +1,24 @@
+require_relative '../../lib/special_offer'
+
+
+describe SpecialOffer do
+
+  let(:mockItemClass) { double :ItemClass}
+  let(:mockItem1) { double :Item}
+  let(:mockItem2) { double :Item}
+  let(:mockItem3) { double :Item}
+  let(:mockItem4) { double :Item}
+
+  describe '#total_discount' do
+    it 'calculates a zero discount for this basket' do
+      allow(mockItem1).to receive(:item).and_return('apple')
+      allow(mockItem2).to receive(:item).and_return('banana')
+      allow(mockItem3).to receive(:item).and_return('melon')
+      allow(mockItem4).to receive(:item).and_return('lime')
+      zero_discount_basket = [mockItem1, mockItem1, mockItem2, mockItem4, mockItem3]
+      discount = SpecialOffer.new(zero_discount_basket)
+      expect(discount.discount).to eq(0)
+    end
+  end
+
+end
